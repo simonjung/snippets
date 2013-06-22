08b2e9f2d7b3052a005c3968c58d1c352729f750
Simon Jung <simon.leo.jung@gmail.com>
2013-06-21 21:56:09 -0400

    Added a snippet that finds user defined global properties on window

---
+++ js/list_user_defined_global_properties_on_window.js
@@ -0,0 +1,16 @@
+// make sure it doesn't count my own properties
+(function () {
+    var results, currentWindow,
+    // create an iframe and append to body to load a clean window object
+    iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    document.body.appendChild(iframe);
+    // get the current list of properties on window
+    currentWindow = Object.getOwnPropertyNames(window);
+    // filter the list against the properties that exist in the clean window
+    results = currentWindow.filter(function(prop) {
+        return !iframe.contentWindow.hasOwnProperty(prop);
+    });
+    // log an array of properties that are different
+    console.log(results);
+}());
